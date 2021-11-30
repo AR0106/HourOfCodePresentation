6aa7e106623f88401387607ef36b0c7419dcdf2e
Alex Ritt <46687522+AR0106@users.noreply.github.com>
2021-11-30 08:06:27 -0600

    Add project files.

---
+++ HourOfCodePresentation/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace HourOfCodePresentation
+{
+    internal class Program
+    {
+        public static int currentPos = 0;
+
+        public static string[] options = {
+            "Option 1",
+            "Option 2",
+            "Option 3",
+            "Option 4"
+            };
+
+        public static void DrawMenu()
+        {
+            Console.Clear();
+
+            for (int i = 0; i < options.Length; i++)
+            {
+                if (i == currentPos)
+                {
+                    // Highlight
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine(options[i]);
+
+                    // Back to Normal
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+
+                else
+                {
+                    Console.WriteLine(options[i]);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+
+            DrawMenu();
+
+            var k = Console.ReadKey();
+
+            while (k.Key != ConsoleKey.Escape)
+            {
+                // Navigate Menu Up
+                if (k.Key == ConsoleKey.UpArrow)
+                {
+                    if (currentPos != 0)
+                    {
+                        currentPos--;
+                    }
+                }
+                // Navigate Menu Down
+                else if (k.Key == ConsoleKey.DownArrow)
+                {
+                    if (currentPos < options.Length - 1)
+                    {
+                        currentPos++;
+                    }
+                }
+
+                DrawMenu();
+
+                k = Console.ReadKey();
+            }
+        }
+    }
+}
